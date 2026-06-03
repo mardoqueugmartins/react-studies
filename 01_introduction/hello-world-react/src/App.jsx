186fbf4c7e8d04dd49332c8b518e9bccda3b9fab
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-06-03 17:47:04 -0300

    primeiro commit

---
+++ 01_introduction/hello-world-react/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+        <h1>Hello World!</h1>
+        <h2></h2>
+    </div>
+
+  )
+}
+
+export default App;
