d021edeebd966ecda795fb7018b4fbefc1098425
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-06-04 12:22:31 -0300

    Eventos de função

--- 03_components/componentes/src/components/Events.jsx
+++ 03_components/componentes/src/components/Events.jsx
@@ -1,10 +1,20 @@
 const Events = () => {
+
+  const handleClick = (e) => {
+    console.log(e);
+    console.log("Executou");
+  }
+
   return (
     <div>
       <div>
         <button onClick={() => console.log("Testando evento de clique.")}>
           Clique aqui.
         </button>
+      </div>
+      {/* Eventos com função */}
+      <div>
+        <button onClick={handleClick}>Clique aqui com função</button>
       </div>
     </div>
   );
