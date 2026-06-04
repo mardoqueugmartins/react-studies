30c1653800a4c267b6b04c40dea74ef17479cac5
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-06-04 12:34:43 -0300

    Função de Renderização

--- 03_components/componentes/src/components/Events.jsx
+++ 03_components/componentes/src/components/Events.jsx
@@ -3,7 +3,16 @@ const Events = () => {
   const handleClick = (e) => {
     console.log(e);
     console.log("Executou");
-  }
+  };
+
+  // Funções de Renderização
+  const renderSomething = (x) => {
+    if (x) {
+      return <h1>Rederizando isso!</h1>
+    } else {
+      return <h1>Renderizando outra coisa.</h1>
+    }
+  };
 
   return (
     <div>
@@ -16,6 +25,9 @@ const Events = () => {
       <div>
         <button onClick={handleClick}>Clique aqui com função</button>
       </div>
+      {/*  Função de renderização */}
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   );
 };
