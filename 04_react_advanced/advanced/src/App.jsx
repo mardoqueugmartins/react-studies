5155bfc17897b2c84b5e2d9e683b84f122119d38
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-06-05 14:35:04 -0300

    Hook useState

--- 04_react_advanced/advanced/src/App.jsx
+++ 04_react_advanced/advanced/src/App.jsx
@@ -3,14 +3,19 @@ import './App.css'
 // Imagem em assets
 import night from './assets/night.jpg'
 
+// useState
+import Data from './components/Data';
+
 function App() {
   return (
-    <div className='App'>
+    <div className='App' style={{paddingBottom: "50px"}}>
       <h1>Avançando em React</h1>
       {/* Imagem em public */}
       <img src="/img.jpg" alt="Street" />
       {/* Imagem em assets */}
       <img src={night} alt="Night" />
+      {/* useState */}
+      <Data />
     </div>
   );
 }
