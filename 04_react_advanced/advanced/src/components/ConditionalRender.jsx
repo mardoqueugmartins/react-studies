f1059b1950c830e4dc5ab612e845187e6ed52e1b
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-06-08 14:27:52 -0300

    Props do componente

--- 04_react_advanced/advanced/src/components/ConditionalRender.jsx
+++ 04_react_advanced/advanced/src/components/ConditionalRender.jsx
@@ -3,11 +3,24 @@ import React from "react";
 const ConditionalRender = () => {
   const x = 11;
 
+  const name = "Mardoqueu";
+
   return (
     <div>
       {/*  Render Condicional */}
       <h3>Isso será exibido?</h3>
       {x > 10 && <p>Se x for true, sim!</p>}
+      {/* Else condicional */}
+      <h3>Render ternário:</h3>
+      {name === "Mardoqueu" ?(
+        <div>
+            <p>Olá Mardoqueu!</p>
+        </div>
+      ) : (
+        <div>
+            <p>Nome não encontrado!</p>
+        </div>
+      )}
     </div>
   );
 };
