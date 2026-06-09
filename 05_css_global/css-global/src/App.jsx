0da30e2e4fc420ae2f880abfea5ec078dcf80120
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-06-09 15:52:25 -0300

    CSS Inline Dinâmico

--- 05_css_global/css-global/src/App.jsx
+++ 05_css_global/css-global/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 // 2 - CSS de Componente
 import MyComponents from './components/MyComponents'
 
+// 4 - CSS Inline Dinâmico
+const n = 15;
+
 function App() {
   return (
     <div>
@@ -11,8 +14,19 @@ function App() {
       {/* 2 - CSS de Componente */}
       <MyComponents />
       <p>Pegou o CSS do componente.</p>
+      {/* 3 - Inline Style */}
+      <p style={{ color: "gray", padding: "20px 20px 20px 0" }}>
+        Este elemento tem estilos inline.
+      </p>
+      {/* 4 - CSS Inline Style Dinâmico */}
+      <h2 style={n > 10 ? { color: "green" } : { color: "red" }}>
+        CSS Inline Dinâmico
+      </h2>
+      <h2 style={n > 20 ? { color: "green" } : { color: "red" }}>
+        CSS Inline Dinâmico
+      </h2>
     </div>
-  )
+  );
 }
 
 export default App
