2388bd2bea98c71412ede63fba681210813fb708
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-06-09 14:22:29 -0300

    CSS de Componente

--- 05_css_global/css-global/src/App.jsx
+++ 05_css_global/css-global/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 
-function App() {
-  
+// 2 - CSS de Componente
+import MyComponents from './components/MyComponents'
 
+function App() {
   return (
     <div>
       {/* 1 - CSS global */}
       <h1>CSS no React</h1>
+      {/* 2 - CSS de Componente */}
+      <MyComponents />
+      <p>Pegou o CSS do componente.</p>
     </div>
   )
 }
---
+++ 05_css_global/css-global/src/components/MyComponents.jsx
@@ -0,0 +1,12 @@
+import './MyComponent.css';
+
+const MyComponents = () => {
+  return (
+    <div>
+      <h1>CSS de Componente</h1>
+      <p>Este parágrafo vai ter outro estilo.</p>
+    </div>
+  )
+}
+
+export default MyComponents
